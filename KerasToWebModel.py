40a9b474b2a46f93d2f62ec68a84d83d0d9a879e
Frost-Lord <ewen2076@gmail.com>
2025-04-24 16:47:38 +1000

    Update KerasToWebModel.py

--- KerasToWebModel.py
+++ KerasToWebModel.py
@@ -1,8 +1,5 @@
 from tensorflow import keras
 import tensorflowjs as tfjs
 
-# Load the .keras model
 model = keras.models.load_model("model.keras")
-
-# Convert to tfjs format (creates model.json and binary .bin weights)
 tfjs.converters.save_keras_model(model, "./web_model")
